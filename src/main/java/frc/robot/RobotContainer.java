5d695b6a4911a46cbf56978c7dffeef608f65421
Austin Mathes <35355378+JimB00nie@users.noreply.github.com>
2022-03-05 16:04:46 -0800

    Updated Libraries - Started working on auto
    Updated Swerve Lib and started working on auto drive

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -12,7 +13,7 @@ public class Climber extends SubsystemBase {
   private DoubleSolenoid ClimbCylinders;
 
   public Climber() {
-    ClimbCylinders = new DoubleSolenoid(2,null, 4,5);
+    ClimbCylinders = new DoubleSolenoid(2,PneumaticsModuleType.REVPH, 4, 5);
     ClimbCylinders.set(Value.kOff);
   }
 
